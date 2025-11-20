15978749baa47be7460d5d8fa5c7782694e78701
AlienQ7 <celestialq7@gmail.com>
2025-11-20 16:41:28 +0530

    Create memories.js

---
+++ memories.js
@@ -0,0 +1,23 @@
+// memories.js
+
+const MEMORIES = [
+    {
+        id: 1,
+        date: "Our first date at the coffee shop.",
+        text: "It was raining outside, but inside, we talked for hours. I knew then I had found someone special.",
+        image: "images/coffee-shop.jpg" // Optional: Path to a photo
+    },
+    {
+        id: 2,
+        date: "The day we adopted the cat.",
+        text: "Watching you cuddle with 'Mittens' for the first time was one of the sweetest moments ever.",
+        image: "images/mittens.jpg"
+    },
+    {
+        id: 3,
+        date: "That disastrous camping trip...",
+        text: "The tent collapsed, it poured, but we laughed the whole time. Best bad trip ever.",
+        image: "images/camping.jpg"
+    }
+    // Add 10-20 more memories here!
+];
