813ccd0fe382958f60198701407f5214ef2a0b6e
AlienQ7 <celestialq7@gmail.com>
2025-11-20 17:03:29 +0530

    Update memories.js

--- memories.js
+++ memories.js
@@ -1,33 +1,17 @@
 // memories.js
 
-const MEMORIES = [
+export const SLIDES = [
     {
-        id: 1,
-        date: "Our first date at the coffee shop.",
-        text: "It was raining outside, but inside, we talked for hours. I knew then I had found someone special.",
-        image: "images/coffee-shop.jpg" // Optional: Path to a photo
+        caption: "Our first date photo! We were so nervous.",
+        image: "slide1.jpg" 
     },
     {
-        id: 2,
-        date: "The day we adopted the cat.",
-        text: "Watching you cuddle with 'Mittens' for the first time was one of the sweetest moments ever.",
-        image: "images/mittens.jpg"
+        caption: "The view from that mountain hike we took last summer.",
+        image: "slide2.jpg"
     },
     {
-        id: 3,
-        date: "That disastrous camping trip...",
-        text: "The tent collapsed, it poured, but we laughed the whole time. Best bad trip ever.",
-        image: "images/camping.jpg"
+        caption: "The day we got our matching cat sweaters. Best decision ever!",
+        image: "slide3.jpg"
     }
-    // Add 10-20 more memories here!
-];
-export const MEMORIES = [
-    // Ensure you have at least 5 memories here for variety
-    {
-        id: 1,
-        date: "Our first date!",
-        text: "It was a disaster, but I wouldn't trade it for anything.",
-        image: "images/date.jpg"
-    },
-    // ... add more ...
+    // Add more slides here!
 ];
